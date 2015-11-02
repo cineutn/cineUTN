ffe6d1934a047629ff8091e9959d15a6d0b19300
nikobm90 <nikobm90@hotmail.com>
2015-11-02 07:13:15 -0300

    ticket

---
+++ partials/entrada.php
@@ -0,0 +1,22 @@
+<link rel="stylesheet" href="../assets/css/bootstrap.min.css">
+<link rel="stylesheet" href="../assets/css/entrada.css">
+
+<div class="entrada">
+	<div class="clienteHeader">
+		<label>SALA</label>
+		<span>8</span>
+	</div>
+	<div class="empleadoHeader">
+		<label>SALA</label>
+		<span>8</span>
+	</div >
+	<div class="clienteFooter">
+		<label >SALA</label>
+		<span>8</span>
+	</div>
+	<div class="empleadoFooter">
+		<label >SALA</label>
+		<span>8</span>
+	</div>
+	
+</div>
